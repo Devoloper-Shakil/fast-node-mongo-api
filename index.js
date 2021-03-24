a882fcae8dc9ab0db05ef0d5281e8a39636031b8
shakil hasan <shshakilhasan69@gmail.com.com>
2021-03-23 22:45:52 -0700

    problem not solve

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-var express = require('express')
-var app = express()
+const express = require('express')
 
+
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express() 
+
+app.use(cors())
+app.use(bodyParser.json())
+
+ 
+const users=['shakil','hemel','naime','amamin']
 app.get('/',(req, res) =>{
   res.send('The start off know a start a Node ......YAea.....')
 })
 
-app.get ('/foots/banena', (req,res)=>{
+app.get ('/foots', (req,res)=>{
   const fotts={
   '1':{'name':'banena', 'price':'100$'},
   '2':{'name':'amrala', 'price':'20$'},
@@ -24,6 +33,18 @@ app.get ('/foots/banena', (req,res)=>{
 
 }
   res.send(fotts);
+})
+
+app.get('/user/:id',(req, res)=>{
+  const id= req.params.id;
+  const userName=users[id];
+  res.send({id,userName})
+  console.log(id);  
+}) 
+app.post('/adduser',(req, res)=>{
+  // res.send(req.body);
+  
+  console.log('data resive', req.body)
 })
 
 app.listen(3000, () =>console.log("the server prot 3000"))
